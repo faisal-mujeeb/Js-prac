f92ba1502464540421f0311d124bb6b65877b59e
Faisal Mujeeb <107312668+faisal-mujeeb@users.noreply.github.com>
2023-01-11 00:07:39 +0530

    Add files via upload

---
+++ higherorderarguments.js
@@ -0,0 +1,13 @@
+function add(numa , numb){
+    return a+b;
+}
+
+function multiply(numa , numb){
+    return a*b;
+}
+
+function calculator (numa, numb, operator){
+    return operator (numa ,numb);
+}
+calculator(2 ,3 , add);
+
